d84c1c58b9f13a3bbe7dc98df1937ea934d24281
Guillaume Michel <GuillaumeMichel02@users.noreply.github.com>
2026-01-03 23:56:36 +0100

    Add corner directions

--- src/core/hex_math/hex_directions.hpp
+++ src/core/hex_math/hex_directions.hpp
@@ -11,13 +11,23 @@
 namespace hexmath {
 
     inline const Vec2i DIRECTIONS[6] = {Vec2i(1,0), Vec2i(1,-1), Vec2i(0,-1), Vec2i(-1,0), Vec2i(-1,1),Vec2i(0,1)};
+
+    inline const Vec2i CORNER_DIRECTIONS[6] = {
+        Vec2i(DIRECTIONS[0] + DIRECTIONS[1]),
+        Vec2i(DIRECTIONS[1] + DIRECTIONS[2]),
+        Vec2i(DIRECTIONS[2] + DIRECTIONS[3]),
+        Vec2i(DIRECTIONS[3] + DIRECTIONS[4]),
+        Vec2i(DIRECTIONS[4] + DIRECTIONS[5]),
+        Vec2i(DIRECTIONS[5] + DIRECTIONS[0])
+    };
+
     inline const Vec2f CORNERS[6] = {
-        Vec2f(DIRECTIONS[0] + DIRECTIONS[1]) * (1.0f/3),
-        Vec2f(DIRECTIONS[1] + DIRECTIONS[2]) * (1.0f/3),
-        Vec2f(DIRECTIONS[2] + DIRECTIONS[3]) * (1.0f/3),
-        Vec2f(DIRECTIONS[3] + DIRECTIONS[4]) * (1.0f/3),
-        Vec2f(DIRECTIONS[4] + DIRECTIONS[5]) * (1.0f/3),
-        Vec2f(DIRECTIONS[5] + DIRECTIONS[0]) * (1.0f/3)
+        Vec2f(CORNER_DIRECTIONS[0]) * (1.0f/3),
+        Vec2f(CORNER_DIRECTIONS[1]) * (1.0f/3),
+        Vec2f(CORNER_DIRECTIONS[2]) * (1.0f/3),
+        Vec2f(CORNER_DIRECTIONS[3]) * (1.0f/3),
+        Vec2f(CORNER_DIRECTIONS[4]) * (1.0f/3),
+        Vec2f(CORNER_DIRECTIONS[5]) * (1.0f/3)
     };
 
 } // namespace hexmath
